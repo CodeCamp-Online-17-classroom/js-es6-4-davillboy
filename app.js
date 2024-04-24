20be5fcc5537363e281b33f5c388f0aad61b9d96
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-24 16:07:27 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 // write code here
+const nums1 = [1, -2, 3, 4];
+const nums2 = [8, 3, -8, 1];
+const combinedArray = [5, ...nums1, -6, -1, ...nums2];
+const sum = combinedArray.reduce((total, num) => total + num, 0);
+
+console.log(combinedArray); 
+console.log(sum); 
